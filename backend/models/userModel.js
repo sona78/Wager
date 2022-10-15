49e2fc2eacbd7ef25d77c0e6d643f05cd060da2c
Anusha Murali <anushamurali@wireless-10-145-51-37.public.utexas.edu>
2022-10-15 14:22:28 -0500

    Backend user Auth

---
+++ backend/models/userModel.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// static signup method
+userSchema.statics.signup = async function (
+  firstname,
+  lastname,
+  email,
+  password
+) {
+  //validation
+  if (!firstname || !lastname || !email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const user = await this.create({
+    firstname,
+    lastname,
+    email,
+    password,
+  });
+
+  return user;
+};
+
+//static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  if (password != user.password) {
+    throw Error("Incorect password");
+  }
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
---
+++ backend/server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+//const path = require("path");
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use("/api/users", userRoutes);
+
+//Serve frontend
+/* _dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+} */
+
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000!");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
