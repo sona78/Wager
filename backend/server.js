1b3c6995382a025fb7a899e2d49149d9faf5a4fb
Anusha Murali <anushamurali@wireless-10-145-51-37.public.utexas.edu>
2022-10-15 23:06:22 -0500

    Menial backend cors work

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 //const path = require("path");
@@ -9,6 +10,7 @@ const app = express();
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
